fd43c44cfe47d15cd4b7d2e5e0728f0dc2775f9d
fahimhasandev <fhasan5@wgu.edu>
2026-06-14 16:31:52 -0500

    Add Navigation feature

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,23 @@
-export default function Home() {
-  return <div>Hello</div>;
+import Link from "next/link";
+
+export default function HomePage() {
+  return (
+    <div className="min-h-screen bg-white">
+      {/* Navigation */}
+      <nav className="border-b border-gray-200 bg-white sticky top-0 z-50 shadow-sm">
+        <div className="container max-auto px-4 py-4 ">
+          <div className="flex items-center justify-between">
+            <Link href="/" className="flex items-center space-x-2">
+              <div className="w-10 h-10 bg-linear-to-r from-indigo-600 to-purple-600 rounded-xl flex items-center justify-center shadow-lg">
+                <span className="text-white font-bold text-2xl">B</span>
+              </div>
+              <span className="text-2xl font-bold bg-linear-to-r from-indigo-600 to-purple-600 bg-clip-text text-transparent">
+                BookWise
+              </span>
+            </Link>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
 }
